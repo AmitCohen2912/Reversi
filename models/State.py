709d000a7692a0e09dd0488afb051ed99bfb1061
Amit Cohen <airamit90@gmail.com>
2026-06-13 20:09:30 +0300

    Implement state display

--- models/State.py
+++ models/State.py
@@ -18,3 +18,11 @@ class State:
         board[4][3] = PLAYER2
         board[4][4] = PLAYER1
         return State(board, PLAYER1)
+
+    def display(self):
+        for row in self.board:
+            for i, col in enumerate(row):
+                if i == 7:
+                    print(col)
+                else:
+                    print(col, end=' ')
